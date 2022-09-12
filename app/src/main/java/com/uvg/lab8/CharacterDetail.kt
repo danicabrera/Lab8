99f1e50724a76a43eb22c9c385c8506f8dc15253
danicabrera <69865919+danicabrera@users.noreply.github.com>
2022-09-11 23:05:42 -0600

    Cambios

---
+++ app/src/main/java/com/uvg/lab8/CharacterDetail.kt
@@ -0,0 +1,11 @@
+package com.uvg.lab8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CharacterDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_detail)
+    }
+}
